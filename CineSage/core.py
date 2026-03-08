7a33b161e2f391e905fa0ef944c70e224376ac8a
nileshwari177 <gawandenn@rknec.edu>
2026-03-08 16:16:08 +0530

    Add structured output using PydanticOutputParser

--- CineSage/core.py
+++ CineSage/core.py
@@ -1,64 +1,103 @@
 from dotenv import load_dotenv
 from langchain_core.prompts import ChatPromptTemplate
+from pydantic import BaseModel
+from typing import List,Optional
+from langchain_core.output_parsers import PydanticOutputParser  
 
 import os
 
 load_dotenv()
 
+
 from langchain_mistralai import ChatMistralAI
 
-model=ChatMistralAI(model='mistral-small-2506')   
+model=ChatMistralAI(model='mistral-small-2506') 
+
+
+# Define the Pydantic model for structured output
+class MovieInfo(BaseModel):
+    movie_name: Optional[str]
+    director: Optional[str]
+    release_year: Optional[str]
+    genre: Optional[str]
+    main_cast: Optional[List[str]]
+    plot_overview: Optional[str]
+    key_themes: Optional[List[str]]
+    rating: Optional[str]
+    soundtrack_composer: Optional[str]
+    notable_features: Optional[List[str]]
+    short_summary: Optional[str]
+
+
+parser = PydanticOutputParser(pydantic_object=MovieInfo)    
+
+
+#promt template
+
+# prompt=ChatPromptTemplate.from_messages([
+#     ("system", """You are an intelligent information extraction assistant.
+
+# Your task is to read the paragraph carefully and extract the most useful information from it.
+
+# Extract the following details if they are present:
+# - Movie Name
+# - Director
+# - Release Year
+# - Genre
+# - Main Cast
+# - Plot / Story Overview
+# - Key Themes
+# - Rating
+# - Music / Soundtrack Composer
+# - Notable Features (visuals, awards, scientific accuracy, etc.)
+
+# After extracting the information, also generate a **short summary (2–3 sentences)** of the paragraph.
+
+# Instructions:
+# - Only extract information that exists in the paragraph.
+# - If some information is missing, write "Not Mentioned".
+# - Keep the response clean and structured.
+
+# Paragraph:
+# {paragraph}
+
+# Return the result in the following format:
+
+# Movie Name:
+# Director:
+# Release Year:
+# Genre:
+# Main Cast:
+# Plot / Story Overview:
+# Key Themes:
+# Rating:
+# Soundtrack Composer:
+# Notable Features:
+
+# Short Summary:"""
+#      ),
+#     ("human", """ Extract information from the following paragraph:
+
+# {paragraph}""")
+
+# ])
 
 prompt=ChatPromptTemplate.from_messages([
-    ("system", """You are an intelligent information extraction assistant.
+    ("system", 
+     """ Extract the most useful information about a movie from the given paragraph.
+     {format_instructions}"""
+        ),
+    ("human", """ Here is the paragraph:{paragraph}""")])
 
-Your task is to read the paragraph carefully and extract the most useful information from it.
-
-Extract the following details if they are present:
-- Movie Name
-- Director
-- Release Year
-- Genre
-- Main Cast
-- Plot / Story Overview
-- Key Themes
-- Rating
-- Music / Soundtrack Composer
-- Notable Features (visuals, awards, scientific accuracy, etc.)
-
-After extracting the information, also generate a **short summary (2–3 sentences)** of the paragraph.
-
-Instructions:
-- Only extract information that exists in the paragraph.
-- If some information is missing, write "Not Mentioned".
-- Keep the response clean and structured.
-
-Paragraph:
-{paragraph}
-
-Return the result in the following format:
-
-Movie Name:
-Director:
-Release Year:
-Genre:
-Main Cast:
-Plot / Story Overview:
-Key Themes:
-Rating:
-Soundtrack Composer:
-Notable Features:
-
-Short Summary:"""
-     ),
-    ("human", """ Extract information from the following paragraph:
-
-{paragraph}""")
-
-])
 
 para=input("Enter a paragraph about a movie: ")
-final_prompt=prompt.invoke({"paragraph": para})
+final_prompt=prompt.invoke({"paragraph": para,
+                            "format_instructions": parser.get_format_instructions()
+                            })
 
 response = model.invoke(final_prompt)
-print(response.content)
+
+
+print(
+response.content
+)
